42b8635b770755c655d36932687d9f2b8ef4785e
anggiedimasta <anggiedimasta@gmail.com>
2025-06-04 21:49:06 +0700

    fix(workspace): remove leftover config workspace

--- packages/ui/nuxt3/.storybook/preview.ts
+++ packages/ui/nuxt3/.storybook/preview.ts
@@ -1,5 +1,5 @@
 import type { Preview } from '@storybook/vue3-vite';
-import '@anggiedimasta/config/main.css';
+import '../src/assets/css/globals.css';
 
 const preview: Preview = {
   parameters: {
